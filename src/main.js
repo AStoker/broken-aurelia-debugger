b3142c584d85738b6a9e2ca7a8aaf2b5871736a8
Andrew Stoker <TheAndyBob@me.com>
2015-11-12 13:58:22 -0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import 'bootstrap';
+import {LogManager} from 'aurelia-framework';
+import {ConsoleAppender} from 'aurelia-logging-console';
+
+LogManager.addAppender(new ConsoleAppender());
+LogManager.setLevel(LogManager.logLevel.debug);
+
+export function configure(aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .developmentLogging()
+    .plugin('astoker/aurelia-debugger');
+
+  //Uncomment the line below to enable animation.
+  //aurelia.use.plugin('aurelia-animator-css');
+
+  //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
+  //aurelia.use.plugin('aurelia-html-import-template-loader')
+
+  aurelia.start().then(a => a.setRoot());
+}
---
+++ src/tab-nav/tab-nav.js
@@ -0,0 +1,8 @@
+import {customElement, bindable} from 'aurelia-framework';
+
+
+@customElement('tab-nav')
+export class tabNav{
+	 @bindable navigation;
+
+}
